e89f98beb6f836ea75d9c069b0fdfd20760babb8
thebigmunch <mail@thebigmunch.me>
2019-08-20 18:10:22 -0400

    Use nox

---
+++ noxfile.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+import nox
+
+py36 = '3.6'
+py37 = '3.7'
+py38 = '3.8'
+
+ON_TRAVIS = 'TRAVIS' in os.environ
+
+
+@nox.session(python=py37)
+def lint(session):
+	session.install('.[lint]')
+	session.run('flake8', 'src/')
+
+
+@nox.session(python=py37)
+def doc(session):
+	shutil.rmtree('docs/_build', ignore_errors=True)
+	session.install('.[doc]')
+	session.cd('docs')
+	session.run(
+		'sphinx-build',
+		'-b',
+		'html',
+		'-W',
+		'-d',
+		'_build/doctrees',
+		'.',
+		'_build/html'
+	)
