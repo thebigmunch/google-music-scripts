b401a3f9fa46a0ced3e51c5cd342552cf2f01d8f
thebigmunch <mail@thebigmunch.me>
2019-10-14 11:02:03 -0400

    Reuse existing venv for all nox sessions

--- noxfile.py
+++ noxfile.py
@@ -3,20 +3,22 @@ import shutil
 
 import nox
 
+ON_TRAVIS = 'TRAVIS' in os.environ
+
 py36 = '3.6'
 py37 = '3.7'
 py38 = '3.8'
 
-ON_TRAVIS = 'TRAVIS' in os.environ
+nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session(reuse_venv=True)
+@nox.session
 def lint(session):
 	session.install('-U', '.[lint]')
 	session.run('flake8', 'src/')
 
 
-@nox.session(reuse_venv=True)
+@nox.session
 def doc(session):
 	shutil.rmtree('docs/_build', ignore_errors=True)
 	session.install('-U', '.[doc]')
